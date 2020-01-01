ac3136c722c1ad636feaafb3d19b3a792b41efac
song <sonleee@qq.com>
2020-01-01 17:00:05 +0800

    完成添加头信息函数，资源不存在函数，请求方法错误处理函数

--- httpd.c
+++ httpd.c
@@ -32,6 +32,33 @@ void execute_cgi(int, const char *, const char *, const char *);
 //如果不是cgi文件，直接读取文件返回给请求http的客户端
 void serve_file(int, const char *);
 
+
+//错误请求
+void bad_request(int);
+
+//读取文件
+void cat(int, FILE *);
+
+//无法执行
+void cannot_execute(int);
+
+//错误输出
+void error_die(const char *);
+
+//得到一行数据,只要发现c为\n,就认为是一行结束，如果读到\r,再用MSG_PEEK的方式读入一个字符，如果是\n，从socket用读出
+//如果是下个字符则不处理，将c置为\n，结束。如果读到的数据为0中断，或者小于0，也视为结束，c置为\n
+int get_line(int, char *, int);
+
+//返回http头
+void headers(int, const char *);
+
+//没有发现文件
+void not_found(int);
+
+//如果不是Get或者Post，就报方法没有实现
+void unimplemented(int);
+
+
 //接收客户端的连接，并读取请求数据
 void *accept_request(void* from_client)
 {
@@ -156,6 +183,57 @@ void *accept_request(void* from_client)
 	 close(client);
 	 return NULL;
 }
+
+void bad_request(int client)
+{
+	char buf[1024];
+	sprintf(buf, "HTTP/1.0 400 BAD REQUEST\r\n");
+	send(client, buf, sizeof(buf), 0);
+	sprintf(buf, "Content-type: text/html\r\n");
+	send(client, buf, sizeof(buf), 0);
+	sprintf(buf, "\r\n");
+	send(client, buf, sizeof(buf), 0);
+	sprintf(buf, "<P>Your browser sent a bad request, ");
+	send(client, buf, sizeof(buf), 0);
+	sprintf(buf, "such as a POST without a Content-Length.\r\n");
+	send(client, buf, sizeof(buf), 0);
+}
+
+//得到内容，发送
+void cat(int client, FILE *resource)
+{
+	char buf[1024];
+
+	//从文件文件描述符中读取指定内容
+	fgets(buf, sizeof(buf), resource);
+	while (!feof(resource))
+	{
+		send(client, buf, strlen(buf), 0);
+		fgets(buf, sizeof(buf), resource);
+	}
+}
+
+void cannot_execute(int client)
+{
+ char buf[1024];
+
+	sprintf(buf, "HTTP/1.0 500 Internal Server Error\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "Content-type: text/html\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<P>Error prohibited CGI execution.\r\n");
+	send(client, buf, strlen(buf), 0);
+}
+
+void error_die(const char *sc)
+{
+ //包含于<stdio.h>,基于当前的 errno 值，在标准错误上产生一条错误消息。参考《TLPI》P49
+ perror(sc); 
+ exit(1);
+}
+
 
 
 //调用cgi
@@ -301,6 +379,78 @@ void execute_cgi(int client, const char *path,const char *method, const char *qu
 	}
 }
 
+int get_line(int sock, char *buf, int size)
+{
+	int i = 0;
+	char c = '\0';
+	int n;
+	while ((i < size - 1) && (c != '\n'))
+	{
+		//recv()包含于<sys/socket.h>,参读《TLPI》P1259,
+		//读一个字节的数据存放在 c 中
+		n = recv(sock, &c, 1, 0);
+		if (n > 0)
+		{
+			if (c == '\r')
+			{
+				//如果是\n就读走
+				n = recv(sock, &c, 1, MSG_PEEK);
+				if ((n > 0) && (c == '\n'))
+					recv(sock, &c, 1, 0);
+				else
+				//不是\n（读到下一行的字符）或者没读到，置c为\n 跳出循环,完成一行读取
+					c = '\n';
+			}
+			buf[i] = c;
+			i++;
+		}
+		else
+		  c = '\n';
+	}
+	buf[i] = '\0';
+	return(i);
+}
+//加入http的headers
+void headers(int client, const char *filename)
+{
+	char buf[1024];
+	(void)filename;  /* could use filename to determine file type */
+
+	strcpy(buf, "HTTP/1.0 200 OK\r\n");
+	send(client, buf, strlen(buf), 0);
+	strcpy(buf, SERVER_STRING);
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "Content-Type: text/html\r\n");
+	send(client, buf, strlen(buf), 0);
+	strcpy(buf, "\r\n");
+	send(client, buf, strlen(buf), 0);
+}
+//如果资源没有找到得返回给客户端下面的信息
+void not_found(int client)
+{
+	char buf[1024];
+
+	sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, SERVER_STRING);
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "Content-Type: text/html\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "your request because the resource specified\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "is unavailable or nonexistent.\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "</BODY></HTML>\r\n");
+	send(client, buf, strlen(buf), 0);
+}
+
+
 //如果不是CGI文件，直接读取文件返回给请求的http客户端
 void serve_file(int client, const char *filename)
 {
@@ -367,6 +517,28 @@ int startup(u_short *port)
 	 *port = ntohs(name.sin_port);
 	}
 
+}
+
+//如果请求方法没有实现，就返回此信息
+void unimplemented(int client)
+{
+	char buf[1024];
+	sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, SERVER_STRING);
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "Content-Type: text/html\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "</TITLE></HEAD>\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "</BODY></HTML>\r\n");
+	send(client, buf, strlen(buf), 0);
 }
 
 
